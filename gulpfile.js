340f1b3ba8a0807d85275766f05f1cfa8e987235
brandonxiang <1542453460@qq.com>
2017-05-25 19:07:20 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,26 @@
-/**
- * Created by xiehui493 on 2016/12/9.
- */
-var gulp = require('gulp');
-var less = require('gulp-less');
-var postcss = require('gulp-postcss');
-var px2rem = require('postcss-px2rem');// px转rem插件
-var base64 = require('gulp-base64');  // base64转码
-var minify = require('gulp-minify-css');// css压缩
-var rimraf = require('gulp-rimraf'); // 删除文件
+var gulp = require('gulp')
+var less = require('gulp-less')
+var postcss = require('gulp-postcss')
+var px2rem = require('postcss-px2rem')
+var base64 = require('gulp-base64')
+var minify = require('gulp-minify-css')
+var rimraf = require('gulp-rimraf')
 
-var rev = require('gulp-rev');                           // 添加版本号
-var revCollector = require('gulp-rev-collector');        // 添加版本号
-var browserSync = require('browser-sync');
-var clean = require('gulp-clean');                       // 清理文目标文件夹
-var cache = require('gulp-cache');                       // 缓存处理
-var htmlmin = require('gulp-htmlmin');                    // 压缩html
-var replace = require('gulp-replace');                     // 替换路径
-var uglify = require('gulp-uglify');
+var rev = require('gulp-rev')
+var revCollector = require('gulp-rev-collector')
+var browserSync = require('browser-sync')
+var clean = require('gulp-clean')
+var cache = require('gulp-cache')
+var htmlmin = require('gulp-htmlmin')
+var replace = require('gulp-replace')
+var uglify = require('gulp-uglify')
 
 var config = {
     src: 'src',
     dest: 'dist',
     file: 'hello',
     port: 3000,
-};
+}
 
 var src = './' + config.src + '/' + config.file
 var dest = './' + config.dest + '/' + config.file
@@ -31,7 +28,7 @@ var dest = './' + config.dest + '/' + config.file
 gulp.task('build:rm', function () {
     gulp.src([dest], { read: false })
         .pipe(rimraf())
-});
+})
 
 gulp.task('build:css', function () {
     gulp.src(src + '/**/*.css')
@@ -43,11 +40,11 @@ gulp.task('build:css', function () {
 })
 
 gulp.task('build:less', function () {
-    var processors = [px2rem({ remUnit: 10 })];
+    var processors = [px2rem({ remUnit: 10 })]
     gulp.src(src + '**/*.less')
         .pipe(less().on('error', function (e) {
-            console.error(e.message);
-            this.emit('end');
+            console.error(e.message)
+            this.emit('end')
         }))
         .pipe(postcss(processors))
         .pipe(gulp.dest(dest))
@@ -68,7 +65,7 @@ gulp.task('build:html', function () {
         removeEmptyAttributes: true, // 删除所有空格作属性值 <input id="" /> ==> <input />
         minifyJS: true, // 压缩页面JS
         minifyCSS: true, // 压缩页面CSS
-    };
+    }
     gulp.src(src + '/**/*.html')
         .pipe(htmlmin(options))
         .pipe(gulp.dest(dest))
@@ -85,17 +82,17 @@ gulp.task('build:script', function () {
 gulp.task('release', ['build:style', 'build:static', 'build:html', 'build:script'])
 
 gulp.task('watch', function () {
-    gulp.watch(src + '**/*.less', ['build:less']);
-    gulp.watch(src + '**/*.css', ['build:css']);
-    gulp.watch(src + '**/*.js', ['build:script']);
-    gulp.watch(src + '**/*.html', ['build:html']);
+    gulp.watch(src + '**/*.less', ['build:less'])
+    gulp.watch(src + '**/*.css', ['build:css'])
+    gulp.watch(src + '**/*.js', ['build:script'])
+    gulp.watch(src + '**/*.html', ['build:html'])
 })
 
 gulp.task('server', function () {
     var port = config.port
     browserSync.init({
         server: {
-            baseDir: "./" + config.dest
+            baseDir: './' + config.dest
         },
         ui: {
             port: port + 1,
@@ -104,17 +101,12 @@ gulp.task('server', function () {
             }
         },
         port: port,
-        startPath: '/'+ config.file
-    });
+        startPath: '/' + config.file
+    })
 })
 
 gulp.task('default', ['release'], function () {
     gulp.start('server')
     gulp.start('watch')
 })
-
-
-
-
-
 
